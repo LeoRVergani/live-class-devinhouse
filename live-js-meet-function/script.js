46d3ce5ceddbd68a71a188f5dbe42ff9e9945fb2
leorvergani <vergani@outlook.com.br>
2024-07-30 19:56:59 -0300

    calculadora finalizada

--- live-js-meet-function/script.js
+++ live-js-meet-function/script.js
@@ -3,14 +3,22 @@ function calcularMediaNotas(nota1, nota2, nota3, nota4){
     return mediaNotas;
 };
 
-document.getElementById('calculadora').addEventListener('submit', function (event) {
+function executarFormulario (event) {
     event.preventDefault();
-    var nota1 = document.getElementById('nota1').value;
-    var nota2 = document.getElementById('nota2').value;
-    var nota3 = document.getElementById('nota3').value;
-    var nota4 = document.getElementById('nota4').value;
+    var nota1 = parseFloat(document.getElementById('nota1').value);
+    var nota2 = parseFloat(document.getElementById('nota2').value);
+    var nota3 = parseFloat(document.getElementById('nota3').value);
+    var nota4 = parseFloat(document.getElementById('nota4').value);
 
-});
+    console.log(calcularMediaNotas(nota1, nota2, nota3, nota4).toFixed(2));
+}
+
+// Evento
+document // document é o arquivo que recebe o script
+.getElementById('calculadora') // pegar o elemento com id calculadora
+.addEventListener('submit', executarFormulario);
+
+
 
 /*
 const display = document.getElementById('calculadora');
